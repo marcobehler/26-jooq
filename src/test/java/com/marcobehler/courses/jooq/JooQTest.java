7ef38d60cceadb361adbdb22617e857e852fe02a
marco <marco@marcobehler.com>
2019-04-06 12:42:20 +0200

    1- Maven Project Setup

---
+++ src/test/java/com/marcobehler/courses/jooq/JooQTest.java
@@ -0,0 +1,11 @@
+package com.marcobehler.courses.jooq;
+
+import org.junit.Test;
+
+public class JooQTest {
+
+    @Test
+    public void name() {
+        System.out.println(System.getProperty("java.version"));
+    }
+}
