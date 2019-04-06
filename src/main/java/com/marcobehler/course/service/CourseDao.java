b46e9ecf762afae739bcdc87dfa8bc8f733012bb
marco <marco@marcobehler.com>
2019-04-06 15:18:49 +0200

    7 - pojos and daos

--- src/main/java/com/marcobehler/course/service/CourseDao.java
+++ src/main/java/com/marcobehler/course/service/CourseDao.java
@@ -10,7 +10,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class CourseService {
+public class CourseDao {
 
     public CoursesRecord findById(Integer id) {
         //
--- src/test/java/com/marcobehler/courses/jooq/JooQTest.java
+++ src/test/java/com/marcobehler/courses/jooq/JooQTest.java
@@ -1,6 +1,6 @@
 package com.marcobehler.courses.jooq;
 
-import com.marcobehler.course.service.CourseService;
+import com.marcobehler.course.service.CourseDao;
 import com.marcobehler.courses.jooq.public_.tables.pojos.Courses;
 import org.junit.Test;
 
@@ -9,14 +9,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class JooQTest {
 
     @Test
-    public void saving_our_first_record() {
+    public void saving_our_first_pojo() {
         Courses c = new Courses();
 
         c.setTitle("Hibernate rockz");
         c.setDescription("Hibernate is so much better!");
         c.setPrice((short) 5000);
 
-        Courses pojo = new CourseService().save(c);
+        Courses pojo = new CourseDao().save(c);
         assertThat(pojo.getId()).isNotNull();
     }
 }
