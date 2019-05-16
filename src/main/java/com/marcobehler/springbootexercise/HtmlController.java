19c686807d7fd861c5d97c29baae463b787c736c
marco <marco@marcobehler.com>
2019-05-16 08:59:35 +0200

    finished up exercise

--- src/main/java/com/marcobehler/springbootexercise/HtmlController.java
+++ src/main/java/com/marcobehler/springbootexercise/HtmlController.java
@@ -2,17 +2,25 @@ package com.marcobehler.springbootexercise;
 
 import com.marcobehler.springbootexercise.jooq.public_.tables.daos.ArticlesDao;
 import com.marcobehler.springbootexercise.jooq.public_.tables.pojos.Articles;
+import com.marcobehler.springbootexercise.jooq.public_.tables.records.ArticlesRecord;
+import org.jooq.DSLContext;
+import org.jooq.SelectConditionStep;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import static com.marcobehler.springbootexercise.jooq.public_.tables.Articles.ARTICLES;
+
 @Controller
 public class HtmlController {
 
     @Autowired
     private ArticlesDao articlesDao;
+
+    @Autowired
+    private DSLContext dslContext;
 
     @GetMapping("/")
     public String blog(Model model) {
@@ -23,9 +31,13 @@ public class HtmlController {
 
     @GetMapping("/articles/{id}")
     public String detailView(Model model, @PathVariable Integer id) {
-        Articles article = articlesDao.findById(id);
 
-        model.addAttribute("title", article.getTitle());
+        ArticlesRecord record = dslContext
+                .selectFrom(ARTICLES)
+                .where(ARTICLES.ID.eq(id))
+                .fetchAny();
+
+        model.addAttribute("title", record.getTitle());
         return "blog-detail";
     }
 
